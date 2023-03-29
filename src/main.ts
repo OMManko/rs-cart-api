81ba16df5ab7ced52462712f67d593f8c955d432
Volha_Manko <volha.manko@workhuman.com>
2023-03-29 16:18:21 +0200

    [Task-8] Revert changes in main branch

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
-import helmet from 'helmet';
+import { AppModule } from './app.module';
 
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
   app.use(helmet());
-
+  
   await app.listen(port);
 }
 bootstrap().then(() => {
